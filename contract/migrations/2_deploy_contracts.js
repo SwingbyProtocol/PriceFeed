8d9e11322fb31027a2118e0779ecb3ee61fd45d6
syrohei <syrohei@gmail.com>
2018-08-17 17:33:33 +0800

    fix config of gaslimit

--- contract/migrations/2_deploy_contracts.js
+++ contract/migrations/2_deploy_contracts.js
@@ -4,7 +4,6 @@ module.exports = function (deployer) {
   const pair = "ETH/BTC"
   deployer.deploy(TrustedOracle, pair).then(async () => {
     const oracle = await TrustedOracle.deployed()
-    const add = await oracle.add(oracle.address)
-    console.log(add.tx)
+    const add = await oracle.add(62627279102384291)
   })
 };
